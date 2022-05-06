be40afa2aa559bbb6983fefde0f9f943024cdcd4
Caio Faraleski <caiotadeu_2012@hotmail.com>
2022-05-06 18:31:50 -0300

    faltam detalhes

--- public/bundle.js
+++ public/bundle.js
@@ -9,10 +9,97 @@
 
 window.onload = function () {
   var board = document.querySelector("#table");
-  console.log(board);
   var context = board.getContext("2d");
-  context.fillStyle = "#fcd31f";
-  context.fillRect(0, 0, board.width, board.height);
+  document.addEventListener("keydown", function (event) {
+    switch (event.keyCode) {
+      case 37:
+        veloX = -velo;
+        veloY = 0;
+        break;
+
+      case 38:
+        veloX = 0;
+        veloY = -velo;
+        break;
+
+      case 39:
+        veloX = velo;
+        veloY = 0;
+        break;
+
+      case 40:
+        veloX = 0;
+        veloY = velo;
+        break;
+
+      default:
+        break;
+    }
+  });
+  setInterval(game, 90);
+  var velo = 1;
+  var veloX = 0;
+  var veloY = 0;
+  var initialPointX = 10;
+  var initialPointY = 15;
+  var pieceSize = board.getBoundingClientRect().width / 17;
+  var pieceAmount = 17;
+  var applePointX = 15;
+  var applePointY = 15;
+  var trail = [];
+  tail = 2;
+
+  function game() {
+    console.log("to aqui");
+    initialPointX += veloX;
+    initialPointY += veloY;
+
+    if (initialPointX < 0) {
+      initialPointX = pieceAmount - 1;
+    }
+
+    if (initialPointX > pieceAmount - 1) {
+      initialPointX = 0;
+    }
+
+    if (initialPointY < 0) {
+      initialPointY = pieceAmount - 1;
+    }
+
+    if (initialPointY > pieceAmount - 1) {
+      initialPointY = 0;
+    }
+
+    context.fillStyle = "#fcd31f";
+    context.fillRect(0, 0, board.width, board.height);
+    context.fillStyle = "red";
+    context.fillRect(applePointX * pieceSize, applePointY * pieceSize, pieceSize, pieceSize);
+    context.fillStyle = "blue";
+
+    for (var i = 0; i < trail.length; i++) {
+      context.fillRect(trail[i].x * pieceSize, trail[i].y * pieceSize, pieceSize, pieceSize);
+
+      if (trail[i].x == initialPointX && trail[i].y == initialPointY) {
+        veloX = 0;
+        veloY = 0;
+      }
+    }
+
+    trail.push({
+      x: initialPointX,
+      y: initialPointY
+    });
+
+    while (trail.length > tail) {
+      trail.shift();
+    }
+
+    if (applePointX == initialPointX && applePointY == initialPointY) {
+      tail++;
+      applePointX = Math.floor(Math.random() * pieceAmount);
+      applePointY = Math.floor(Math.random() * pieceAmount);
+    }
+  }
 };
 
 /***/ }),
